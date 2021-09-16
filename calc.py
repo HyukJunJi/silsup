8f78aef71307c7d3c4eada0690a75ec60822e8b5
Arfistro <whrnjsrkdls@gmail.com>
2021-09-16 12:32:14 +0900

    print 문 추가

--- calc.py
+++ calc.py
@@ -3,7 +3,7 @@ def sum_function( n1 , n2):  # add two number
     return n1 + n2
 num1 = int ( input(“ input number 1” ) )
 num2 = int ( input(“ input number 2” ) )
-
+print("hello")
 sum = sum_function( num1 , num2)
 
 print(num1 , “+” , num2 ,  “=” , sum )
